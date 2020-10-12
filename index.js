f7a89af972b6e6b75f840bb5175eb332d42b737c
Sarah Iraurgui <iraurguisj@gmail.com>
2020-10-11 22:52:42 -0400

    seeds file

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const logo = require("asciiart-logo");
 //function init()
 
 function init () {
-    const logoText = logo({ name: "Employee Manager"}.render();
+    const logoText = logo({ name: "Employee Manager"}).render();
     console.log(logoText);
 
     //load our prompts
@@ -18,6 +18,16 @@ function loadPrompts () {
     inquirer.prompt ({
         type: "list",
         name: "choice",
-        message
+        message: "What would you like to do?",
+        choice: [{
+            name: "View All Employees",
+            value: "VIEW_EMPLOYEES"
+        }
+    ]
     })
-}
+
+    //switch statement
+    switch (choice) {
+        case "VIEW_EMPLOYEES":
+            return viewEmployees();
+    }
